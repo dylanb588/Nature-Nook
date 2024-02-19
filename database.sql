17f34dc2efedba4ef537d8af4a63e8332f24d1c5
Dylan Brown <dylanbrown588@gmail.com>
2024-02-19 14:02:55 -0600

    Make the user, plant, and follower table and install MUI

--- database.sql
+++ database.sql
@@ -2,8 +2,28 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
+	"id" SERIAL PRIMARY KEY,
+	"username" varchar(80) UNIQUE NOT NULL,
+	"password" varchar(1000) NOT NULL,
+	"email" varchar(100) UNIQUE NOT NULL,
+	"profile_pic" varchar(300) 
+);
+
+CREATE TABLE "plant" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "user_id" INT REFERENCES "user",
+    "plant_name" varchar(100) NOT NULL,
+    "scientific_name" varchar(150),
+    "plant_image" varchar(300) NOT NULL,
+    "care" TEXT NOT NULL,
+    "soil_type" varchar(60),
+    "water" integer NOT NULL
+);
+
+CREATE TABLE "follower" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user"
+    "follower_id" INT REFERENCES "user"
 );
