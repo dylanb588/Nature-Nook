c8916884979c80ced066ebfe65ce5723d5c4e5fc
Dylan Brown <dylanbrown588@gmail.com>
2024-02-19 14:24:14 -0600

    Make the GET to get that user's plants

--- server/routes/plant.router.js
+++ server/routes/plant.router.js
@@ -6,7 +6,19 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+  const userID = req.user.id
+  const query = `
+  SELECT * FROM "plant"
+  WHERE "user_id" = $1
+  `;
+
+  pool.query(query, [userID])
+  .then((result) => {
+    res.send(result.rows);
+  }).catch((error) => {
+    res.sendStatus(500);
+    console.log('Error getting your plants :(', error);
+  })
 });
 
 /**
