8a05d06091e9faf1da7f0be9ecdce6e4dbdb2042
Dylan Brown <dylanbrown588@gmail.com>
2024-02-19 15:26:57 -0600

    Make the put for updating plant information

--- server/routes/plant.router.js
+++ server/routes/plant.router.js
@@ -2,9 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+// GET for getting the plants associated with that logged in user
 router.get('/', (req, res) => {
   const userID = req.user.id;
   const query = `
@@ -21,9 +19,7 @@ router.get('/', (req, res) => {
   })
 });
 
-/**
- * POST route template
- */
+// POST for adding a new plant
 router.post('/', (req, res) => {
   const userID = req.user.id;
   const { plant_name, scientific_name, plant_image, care, soil_type, water } = req.body;
@@ -41,6 +37,29 @@ router.post('/', (req, res) => {
   }).catch((error) => {
     res.sendStatus(500);
     console.log("Error adding new plant", error);
+  })
+});
+
+
+router.put('/:plantID', (req, res) => {
+  const plantID = req.params.plantID;
+  const userID = req.user.id;
+  const { plant_name, scientific_name, plant_image, care, soil_type, water } = req.body;
+
+  const query = `
+  UPDATE "plant"
+  SET "plant_name" = $1, "scientific_name" = $2, "plant_image" = $3, "care" = $4, "soil_type" = $5, "water" = $6
+  WHERE "id" = $7 AND "user_id" = $8
+  `;
+
+  const values = [plant_name, scientific_name, plant_image, care, soil_type, water, plantID, userID];
+
+  pool.query(query, values)
+  .then(() => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    res.sendStatus(500);
+    console.log("Error updating plant", error);
   })
 });
 
