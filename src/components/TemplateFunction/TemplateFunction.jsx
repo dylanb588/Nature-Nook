0c12e0f5e1df872fdbffb852944652c6fed48dc9
Dylan Brown <dylanbrown588@gmail.com>
2024-02-20 10:59:24 -0600

    Start the add plant form

--- src/components/TemplateFunction/TemplateFunction.jsx
+++ src/components/TemplateFunction/TemplateFunction.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 
+import { TextField, Button } from '@mui/material';
+import Stack from '@mui/material/Stack';
+
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
-function HomePage(props) {
+function AddPlant() {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const plants = useSelector((store) => store.plants);
-  const [heading, setHeading] = useState('Functional Component');
 
   return (
     <div>
-      <h2>{heading}</h2>
+      <form>
+        <Stack>
+          <TextField
+            label="Plant Name"
+            variant="outlined"
+            fullWidth
+            value={}
+            onChange={}
+          />
+        </Stack>
+      </form>
     </div>
   );
 }
 
-export default HomePage;
+export default AddPlant;
