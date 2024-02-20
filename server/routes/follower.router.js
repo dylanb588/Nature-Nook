6b26c0ddb4d032b78b1c001f17600ab92d20ffea
Dylan Brown <dylanbrown588@gmail.com>
2024-02-19 18:36:56 -0600

    Make the follower get to show the user the people they follow

---
+++ server/routes/follower.router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    const userID = req.user.id;
+
+    const query = `
+    SELECT "user"."username" AS follower_username
+    FROM "follower"
+    JOIN "user" ON "follower".follower_id = "user_id"
+    WHERE "follower".user_id = $1
+    `;
+
+    pool.query(query, [userID])
+    .then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        res.sendStatus(500);
+        console.log("Error getting followers", error);
+    })
+})
+
+module.exports = router;
