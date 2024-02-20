f4d01467304a2f834d2a1a28b072d27bb55c5855
Dylan Brown <dylanbrown588@gmail.com>
2024-02-20 09:37:10 -0600

    Make the plant reducer and saga

---
+++ src/redux/sagas/plant.saga.js
@@ -0,0 +1,21 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchPlants() {
+    try{
+        const plantResponse = yield axios.get('/api/plant');
+    
+        yield put({
+            type: 'SET_PLANTS',
+            payload: plantResponse
+        });
+    } catch (error) {
+        console.log("Fetch plants error:", error);
+    }
+}
+
+function* plantSaga() {
+    yield takeLatest('FETCH_PLANTS', fetchPlants);
+}
+
+export default plantSaga;
