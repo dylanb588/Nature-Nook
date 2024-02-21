18422302643ae6bb40787bc97713b3e0717d6620
Dylan Brown <dylanbrown588@gmail.com>
2024-02-21 09:11:22 -0600

    Make the post for note router

--- server/routes/note.router.js
+++ server/routes/note.router.js
@@ -8,9 +8,9 @@ router.get('/:plantID', (req, res) => {
 
     const query = `
         SELECT *
-        FROM note
-        WHERE user_id = $1
-        AND plant_id = $2;
+        FROM "note"
+        WHERE "user_id" = $1
+        AND "plant_id" = $2;
     `;
 
     pool.query(query, [userID, plantID]) // Correct order of parameters
@@ -21,6 +21,25 @@ router.get('/:plantID', (req, res) => {
             console.error("Error getting notes", error);
             res.sendStatus(500);
         });
+});
+
+router.post('/', (req, res) => {
+    const userID = req.user.id;
+    const plantID = req.params.plantID;
+    const note = req.body;
+
+    const query = `
+    INSERT INTO "note" ("user_id", "plant_id", "note")
+    VALUES ($1, $2, $3)
+    `;
+
+    pool.query(query, [userID, plantID, note])
+    .then(result => {
+        res.sendStatus(201);
+    }).catch(error => {
+        res.sendStatus(500);
+        console.log("Error posting note", error);
+    })
 });
 
 module.exports = router;
