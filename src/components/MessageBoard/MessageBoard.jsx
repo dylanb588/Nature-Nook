8cacdd5c339c8aee97203d7c165fcc00b0b21a72
Dylan Brown <dylanbrown588@gmail.com>
2024-02-25 17:09:03 -0600

    Add message input for posting new messages

--- src/components/MessageBoard/MessageBoard.jsx
+++ src/components/MessageBoard/MessageBoard.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Card, CardContent, Typography } from '@mui/material';
+import { useState } from 'react';
+import { TextField, Button } from '@mui/material';
 
 
 function MessageBoard(){
     const messages = useSelector((store) => store.message);
+    const [newMessage, setNewMessage] = useState('');
     const dispatch = useDispatch();
-
-
-    console.log('Here is message', messages);
 
     useEffect(() => {
         dispatch({type: 'FETCH_MESSAGE'})
     }, []);
 
+    function addMessage() {
+        dispatch({type: 'ADD_MESSAGE', payload: newMessage})
+        setNewMessage('');
+    }
+
     const formatDate = (timestamp) => {
         const date = new Date(timestamp);
         const hours = date.getHours();
         const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
         const formattedHours = String(hours).padStart(2, '0'); // Ensure two-digit format for hours
         const ampm = hours >= 12 ? 'PM' : 'AM';
     
@@ -39,6 +43,15 @@ function MessageBoard(){
                 </CardContent>
             </Card>
             ))}
+        <TextField 
+            name='note'
+            label='Add a New Message'
+            value={newMessage}
+            onChange={(event) => setNewMessage(event.target.value)}
+            style={{width: 1000}}
+            required
+        />
+        <Button onClick={() => addMessage()}>Post Message</Button>
     </div>
     )
 }
