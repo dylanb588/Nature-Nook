ba39981d66a9aa050dc3ca688d42a9bd80bdfd00
Dylan Brown <dylanbrown588@gmail.com>
2024-02-25 16:52:12 -0600

    Format the timestamp

--- src/components/MessageBoard/MessageBoard.jsx
+++ src/components/MessageBoard/MessageBoard.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Card, CardContent, Typography } from '@mui/material';
+
 
 function MessageBoard(){
     const messages = useSelector((store) => store.message);
     const dispatch = useDispatch();
+
 
     console.log('Here is message', messages);
 
@@ -11,10 +14,32 @@ function MessageBoard(){
         dispatch({type: 'FETCH_MESSAGE'})
     }, []);
 
-    return(
-        <div>
+    const formatDate = (timestamp) => {
+        const date = new Date(timestamp);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const formattedHours = String(hours).padStart(2, '0'); // Ensure two-digit format for hours
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+    
+        return `${formattedHours}:${minutes} ${ampm}`;
+    };
 
-        </div>
+    return(
+    <div>
+        {messages.map((message, index) => (
+            <Card key={index}>
+                <CardContent>
+                    <Typography variant="body1">
+                        {message.message}
+                    </Typography>
+                    <Typography variant="caption" color="textSecondary">
+                        Posted by: {message.username} at {formatDate(message.posted_at)}
+                    </Typography>
+                </CardContent>
+            </Card>
+            ))}
+    </div>
     )
 }
 
