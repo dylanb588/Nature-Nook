c13a3ca2939d5e96090376423ab5562418490609
Dylan Brown <dylanbrown588@gmail.com>
2024-02-28 14:29:09 -0600

    Filter out logged in user from the search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 
 function Search() {
     const users = useSelector((store)  => store.search);
+    const loggedIn = useSelector(store => store.user);
     const [search, setSearch] = useState('');
     const dispatch = useDispatch();
+
+    console.log(loggedIn);
 
     useEffect(() => {
         dispatch({type: 'FETCH_USERS'})
@@ -20,17 +23,19 @@ function Search() {
                 onChange={(e) => setSearch(e.target.value)}
                 placeholder='Search Users'
             />
-            <ul style={{ listStyleType: 'none' }}> 
+            <ul style={{ listStyleType: 'none' }}>
                 {users.filter((user) => {
-                    return search.toLowerCase() === '' ? user : user.username.toLowerCase().includes(search);
+                    // Filter out the logged-in user's name from the list
+                    return (search.toLowerCase() === '' && user.id !== loggedIn.id) ||
+                        (user.username.toLowerCase().includes(search) && user.id !== loggedIn.id);
                 }).map((user) => (
-                    <Link key={user.id}  to={`/user/${user.id}`}>
+                    <Link key={user.id} to={`/user/${user.id}`}>
                         <li>{user.username}</li>
                     </Link>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
 
 export default Search;
