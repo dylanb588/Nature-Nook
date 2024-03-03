507e206e483748f2bb6de921c2ed475b48bd9c05
Dylan Brown <dylanbrown588@gmail.com>
2024-03-02 18:03:57 -0600

    Style info Page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
+import './InfoPage.css'; // Import CSS file for styling
 
 function InfoPage() {
   return (
-    <div className="container">
-      <h1>Nature Nook Getting Started</h1>
-      <h3>Getting Started</h3>
-      <p>When you first create an account you'll be brought to your nature nook.</p>
-      <p>To fill your Nature Nook with your beautiful plants you'll want to click on Add New Plant in the nav bar. There will also be a starting card on your home page that brings you right to add plant page</p>
-      <p>To foster a commuinty of plant lovers if you navigate to the Search Users youll be able to see other's beautiful Nature Nook's</p>
-      <p>Under the Message Board users can connect with eachother. Try posting about plant care questions or need to get rid of extra soil post it there!</p>
+    <div className="info-container">
+      <h1 className="info-title">Welcome to Nature Nook</h1>
+      <div className="info-section">
+        <h3 className="info-subtitle">Getting Started</h3>
+        <p>As you embark on your journey with Nature Nook, your virtual sanctuary awaits! Upon creating an account, you'll be transported to your very own nature nook.</p>
+        <p>To enrich your Nature Nook experience with vibrant greenery, simply navigate to the "Add New Plant" option in the navigation bar. Alternatively, a convenient card on your homepage will swiftly guide you to the plant addition page.</p>
+        <p>Embark on a journey through the verdant landscapes of fellow plant enthusiasts by exploring the "Search Users" feature. Behold the splendor of others' Nature Nooks!</p>
+        <p>Engage in lively discussions and forge connections within our blossoming community through the "Message Board". Whether seeking advice on plant care or offering surplus soil, this is the place to be!</p>
+      </div>
     </div>
   );
 }
