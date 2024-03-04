70f481d78bfd0a5c445ade3746f793ad00d7105e
Dylan Brown <dylanbrown588@gmail.com>
2024-03-04 09:02:06 -0600

    Style landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -15,45 +15,22 @@ function LandingPage() {
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
+          <h1 className='landTitle'>Welcome to Nature Nook</h1>
+          <p className='landContent'>
+            Discover the joy of nurturing your own green oasis with Nature Nook. Immerse yourself in a world where plant lovers unite to share their passion for all things botanical.
           </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
+          <p className='landContent'>
+            Whether you're a seasoned gardener or just starting out, Nature Nook provides the perfect platform to connect with fellow enthusiasts, exchange plant care tips, and showcase your green thumb.
           </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
+          <p className='landContent'>
+            Join our community today and explore a world of endless botanical wonders. Let's grow together!
           </p>
         </div>
         <div className="grid-col grid-col_4">
-          <RegisterForm />
-
+          <RegisterForm /> {/* Render the RegisterForm component */}
           <center>
             <h4>Already a Member?</h4>
             <button className="btn btn_sizeSm" onClick={onLogin}>
@@ -65,5 +42,6 @@ function LandingPage() {
     </div>
   );
 }
+
 
 export default LandingPage;
